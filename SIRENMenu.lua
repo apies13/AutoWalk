8a04dd98eb514706c5b014ad70e00c8207dca310
r4kshit <fizhafiz1311@gmai.com>
2025-09-18 18:24:05 +0700

    ESP

--- SIRENMenu.lua
+++ SIRENMenu.lua
@@ -872,109 +872,6 @@ do
     })
 end
 
-local LeftGroupBox = Tabs.Aimbot:AddLeftGroupbox("Aimbot & ESP", "crosshair")
--- ========================================
--- Godmode Toggle
--- ========================================
-_G.GodmodeEnabled = false
-
-
--- ESP Section (Sirius Sense)
-local Sense = loadstring(game:HttpGet("https://raw.githubusercontent.com/SiriusSoftwareInc/Sense/main/Library.lua"))()
-local esp = Sense.new("SIRENHub ESP")
-
-local ESPEnabled = false
-local ESPOptions = {
-    Box = true,
-    Line = false,
-    Health = false,
-    Distance = false,
-    Name = false,
-    Weapon = false,
-    Tracer = false,
-    Chams = false,
-}
-
-LeftGroupBox:AddToggle("ESPEnabled", {
-    Text = "Toggle ESP",
-    Tooltip = "Aktifkan/Nonaktifkan ESP",
-    Default = false,
-    Callback = function(Value)
-        ESPEnabled = Value
-        esp.Enabled = Value
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "SIRENHub",
-            Text = Value and "ESP Enabled!" or "ESP Disabled!",
-            Duration = 3
-        })
-    end,
-})
-
-LeftGroupBox:AddToggle("ESPBox", {
-    Text = "Box",
-    Default = true,
-    Callback = function(Value)
-        ESPOptions.Box = Value
-        esp.Box = Value
-    end,
-})
-LeftGroupBox:AddToggle("ESPLine", {
-    Text = "Line",
-    Default = false,
-    Callback = function(Value)
-        ESPOptions.Line = Value
-        esp.Line = Value
-    end,
-})
-LeftGroupBox:AddToggle("ESPHealth", {
-    Text = "Health",
-    Default = false,
-    Callback = function(Value)
-        ESPOptions.Health = Value
-        esp.Health = Value
-    end,
-})
-LeftGroupBox:AddToggle("ESPDistance", {
-    Text = "Distance",
-    Default = false,
-    Callback = function(Value)
-        ESPOptions.Distance = Value
-        esp.Distance = Value
-    end,
-})
-LeftGroupBox:AddToggle("ESPName", {
-    Text = "Name",
-    Default = false,
-    Callback = function(Value)
-        ESPOptions.Name = Value
-        esp.Name = Value
-    end,
-})
-LeftGroupBox:AddToggle("ESPWeapon", {
-    Text = "Weapon",
-    Default = false,
-    Callback = function(Value)
-        ESPOptions.Weapon = Value
-        esp.Weapon = Value
-    end,
-})
-LeftGroupBox:AddToggle("ESPTracer", {
-    Text = "Tracer",
-    Default = false,
-    Callback = function(Value)
-        ESPOptions.Tracer = Value
-        esp.Tracer = Value
-    end,
-})
-LeftGroupBox:AddToggle("ESPChams", {
-    Text = "Chams",
-    Default = false,
-    Callback = function(Value)
-        ESPOptions.Chams = Value
-        esp.Chams = Value
-    end,
-})
-
 
 --===============================
 -- Tabbox Auto Walk Mount Atin
@@ -1197,6 +1094,105 @@ RightGroupBox:AddLabel(
     "Gray = Safe / Usable features (safe to use without restrictions)",
     true
 )
+
+local LeftGroupBox = Tabs.Aimbot:AddLeftGroupbox("Aimbot & ESP", "crosshair")
+
+
+-- ESP Section (Sirius Sense)
+local Sense = loadstring(game:HttpGet("https://raw.githubusercontent.com/SiriusSoftwareInc/Sense/main/Library.lua"))()
+local esp = Sense.new("SIRENHub ESP")
+
+local ESPEnabled = false
+local ESPOptions = {
+    Box = true,
+    Line = false,
+    Health = false,
+    Distance = false,
+    Name = false,
+    Weapon = false,
+    Tracer = false,
+    Chams = false,
+}
+
+local ESPToggle = LeftGroupBox:AddToggle("ESPEnabled", {
+    Text = "Toggle ESP",
+    Tooltip = "Aktifkan/Nonaktifkan ESP",
+    Default = false,
+    Callback = function(Value)
+        ESPEnabled = Value
+        esp.Enabled = Value
+        game.StarterGui:SetCore("SendNotification", {
+            Title = "SIRENHub",
+            Text = Value and "ESP Enabled!" or "ESP Disabled!",
+            Duration = 3
+        })
+    end,
+})
+
+local ESPBox = LeftGroupBox:AddToggle("ESPBox", {
+    Text = "Box",
+    Default = true,
+    Callback = function(Value)
+        ESPOptions.Box = Value
+        esp.Box = Value
+    end,
+})
+local ESPLine = LeftGroupBox:AddToggle("ESPLine", {
+    Text = "Line",
+    Default = false,
+    Callback = function(Value)
+        ESPOptions.Line = Value
+        esp.Line = Value
+    end,
+})
+local ESPHealth = LeftGroupBox:AddToggle("ESPHealth", {
+    Text = "Health",
+    Default = false,
+    Callback = function(Value)
+        ESPOptions.Health = Value
+        esp.Health = Value
+    end,
+})
+local ESPDistance = LeftGroupBox:AddToggle("ESPDistance", {
+    Text = "Distance",
+    Default = false,
+    Callback = function(Value)
+        ESPOptions.Distance = Value
+        esp.Distance = Value
+    end,
+})
+local ESPName = LeftGroupBox:AddToggle("ESPName", {
+    Text = "Name",
+    Default = false,
+    Callback = function(Value)
+        ESPOptions.Name = Value
+        esp.Name = Value
+    end,
+})
+local ESPWeapon = LeftGroupBox:AddToggle("ESPWeapon", {
+    Text = "Weapon",
+    Default = false,
+    Callback = function(Value)
+        ESPOptions.Weapon = Value
+        esp.Weapon = Value
+    end,
+})
+local ESPTracer = LeftGroupBox:AddToggle("ESPTracer", {
+    Text = "Tracer",
+    Default = false,
+    Callback = function(Value)
+        ESPOptions.Tracer = Value
+        esp.Tracer = Value
+    end,
+})
+local ESPChams = LeftGroupBox:AddToggle("ESPChams", {
+    Text = "Chams",
+    Default = false,
+    Callback = function(Value)
+        ESPOptions.Chams = Value
+        esp.Chams = Value
+    end,
+})
 
 
 -- Long text label to demonstrate UI scrolling behaviour.
